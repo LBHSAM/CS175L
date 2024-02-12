f600f399bcbd16f59296eb7aadb7c5446a25b9a8
LBHSAM <96200249+LBHSAM@users.noreply.github.com>
2024-02-12 11:13:58 -0500

    Add files via upload

--- resturants.py
+++ resturants.py
@@ -1,12 +1,14 @@
 #Anthony Mendes
+#cs 102
 #restaurants
 
+
 restaurants = {
-    "Joe’s Gourmet Burgers": {"Vegetarian": False, "Vegan": False, "Gluten-Free": False},
-    "Main Street Pizza Company": {"Vegetarian": True, "Vegan": False, "Gluten-Free": True},
-    "Corner Café": {"Vegetarian": True, "Vegan": True, "Gluten-Free": True},
-    "Mama’s Fine Italian": {"Vegetarian": True, "Vegan": False, "Gluten-Free": False},
-    "The Chef’s Kitchen": {"Vegetarian": True, "Vegan": True, "Gluten-Free": True}
+    "Joes Gourmet Burgers": {"Vegetarian": False, "Vegan": False, "Gluten Free": False},
+    "Main Street Pizza Company": {"Vegetarian": True, "Vegan": False, "Gluten Free": True},
+    "Corner Cafe": {"Vegetarian": True, "Vegan": True, "Gluten Free": True},
+    "Mama’s Fine Italian": {"Vegetarian": True, "Vegan": False, "Gluten Free": False},
+    "The Chef's Kitchen": {"Vegetarian": True, "Vegan": True, "Gluten Free": True}
 }
 
 def filter_restaurants(vegetarian, vegan, gluten_free):
@@ -19,17 +21,13 @@ def filter_restaurants(vegetarian, vegan, gluten_free):
     return choices
 
 def main():
-    repeat = 'yes'
-    while repeat == 'yes':
-        vegetarian = input("Is anyone in your party vegetarian? ").lower() == 'yes'
-        vegan = input("Is anyone in your party vegan? ").lower() == 'yes'
-        gluten_free = input("Is anyone in your party gluten-free? ").lower() == 'yes'
+    vegetarian = input("Is anyone in your party vegetarian? ").lower() == 'yes'
+    vegan = input("Is anyone in your party vegan? ").lower() == 'yes'
+    gluten_free = input("Is anyone in your party gluten-free? ").lower() == 'yes'
 
-        print("Here are your restaurant choices:")
-        for choice in filter_restaurants(vegetarian, vegan, gluten_free):
-            print(choice)
-
-        repeat = input("Enter 'yes' if you would like to do another restaurant search (enter 'no' to end): ").lower()
+    print("Here are your restaurant choices:")
+    for choice in filter_restaurants(vegetarian, vegan, gluten_free):
+        print(choice)
 
 if __name__ == "__main__":
     main()
