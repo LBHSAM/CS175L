8b736040b2149b53679509f84f9ba4b55adac3b0
LBHSAM <96200249+LBHSAM@users.noreply.github.com>
2024-04-18 12:12:02 -0400

    Add files via upload

---
+++ MonthlyExpensesAM.py
@@ -0,0 +1,37 @@
+
+#anthony Mendes
+#cs 175
+
+import matplotlib.pyplot as plt
+
+def read_expenses(file_name):
+    data = {}
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                category, amount = line.strip().split('\t')
+                try:
+                    data[category] = float(amount)
+                except ValueError:
+                    print(f"Error: Invalid amount '{amount}' for category '{category}'")
+    except IOError:
+        print("Error: could Not open or read the Rile.")
+    return data
+
+def plot_pie_chart(expenses):
+    labels = expenses.keys()
+    amounts = expenses.values()
+
+    plt.figure(figsize=(8, 8))
+    plt.pie(amounts, labels=labels)
+    plt.title('Expense Distribution')
+    plt.show()
+
+def main():
+    file_name = 'expenses.txt'
+    expenses = read_expenses(file_name)
+    if expenses:
+        plot_pie_chart(expenses)
+
+if __name__ == "__main__":
+    main()
